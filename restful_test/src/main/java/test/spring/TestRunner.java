2b7116d2c67291d6d9bbb51472302a3070992ce1
Justin Wu <justin01.wu@gmail.com>
2014-12-09 10:20:22 -0500

    add comment for testNG

--- restful_test/src/main/java/test/spring/TestRunner.java
+++ restful_test/src/main/java/test/spring/TestRunner.java
@@ -4,6 +4,11 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
+/**
+ * 
+ * TODO : Change to testNG
+ *
+ */
 public class TestRunner {
 	public static void main(String[] args) {
 		
