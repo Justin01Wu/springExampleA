2507c9a5af2fa93fc8b71921cd0565ec3b60e543
Justin Wu <justin01.wu@gmail.com>
2014-12-17 14:06:56 -0500

    log http response body

--- restful/src/main/java/test/spring/HttpLoggingFilter.java
+++ restful/src/main/java/test/spring/HttpLoggingFilter.java
@@ -2,21 +2,23 @@ package test.spring;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.Map;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
 
+import org.apache.commons.io.output.TeeOutputStream;
+import org.springframework.mock.web.DelegatingServletOutputStream;
 import org.springframework.web.filter.GenericFilterBean;
 
 /**
  * it comes from :
- * http://stackoverflow.com/questions/2158647/logging-response-body
- * -html-from-httpservletresponse-using-spring-mvc-handlerin
+ * http://stackoverflow.com/questions/2158647/logging-response-body-html-from-httpservletresponse-using-spring-mvc-handlerin
  * 
  * @author justin.wu
  *
@@ -24,51 +26,46 @@ import org.springframework.web.filter.GenericFilterBean;
 public class HttpLoggingFilter extends GenericFilterBean {
 
 	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException,
-			ServletException {
-		// HttpServletResponse responseWrapper =
-		// loggingResponseWrapper((HttpServletResponse) response);
-		// filterChain.doFilter(request, responseWrapper);
-		
-		HttpServletRequest httpRequest = (HttpServletRequest) request;
-		HttpServletResponse httpResponse = (HttpServletResponse) response;			
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
 
-		
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+
 		final StringBuilder logMessage = new StringBuilder("   ===>> HTTP Request - ");
 		logMessage.append("[HTTP METHOD:").append(httpRequest.getMethod());
 		String fullURL = getFullURL(httpRequest);
 		logMessage.append("] [URL:").append(fullURL);
-		
+
 		System.out.println(logMessage);
-		
-		filterChain.doFilter(request, response);
+
+		HttpServletResponse httpResponse = (HttpServletResponse) response;
+		// filterChain.doFilter(request, response);
+		HttpServletResponse responseWrapper = loggingResponseWrapper(httpResponse);
+		filterChain.doFilter(request, responseWrapper);
+
 	}
 
-	// private HttpServletResponse loggingResponseWrapper(HttpServletResponse
-	// response) {
-	// return new HttpServletResponseWrapper(response) {
-	// @Override
-	// public ServletOutputStream getOutputStream() throws IOException {
-	// return new DelegatingServletOutputStream(
-	// new TeeOutputStream(super.getOutputStream(), loggingOutputStream())
-	// );
-	// }
-	// };
-	// }
-	
+	private HttpServletResponse loggingResponseWrapper(HttpServletResponse response) {
+		return new HttpServletResponseWrapper(response) {
+			@Override
+			public ServletOutputStream getOutputStream() throws IOException {
+				return new DelegatingServletOutputStream(new TeeOutputStream(super.getOutputStream(), loggingOutputStream()));
+			}
+		};
+	}
+
 	public static String getFullURL(HttpServletRequest request) {
-	    StringBuffer requestURL = request.getRequestURL();
-	    String queryString = request.getQueryString();
+		StringBuffer requestURL = request.getRequestURL();
+		String queryString = request.getQueryString();
 
-	    if (queryString == null) {
-	        return requestURL.toString();
-	    } else {
-	        return requestURL.append('?').append(queryString).toString();
-	    }
+		if (queryString == null) {
+			return requestURL.toString();
+		} else {
+			return requestURL.append('?').append(queryString).toString();
+		}
 	}
 
-	 private OutputStream loggingOutputStream() {
-	 return System.out;
-	 }
+	private OutputStream loggingOutputStream() {
+		return System.out;
+	}
 
 }
