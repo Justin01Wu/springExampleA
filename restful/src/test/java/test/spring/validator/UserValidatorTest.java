af7c13fe806f5d6753beab704f1cb3825f862a2a
Justin Wu <justin01.wu@gmail.com>
2014-12-22 11:54:22 -0500

    improve unit test UserValidatorTest

--- restful/src/test/java/test/spring/validator/UserValidatorTest.java
+++ restful/src/test/java/test/spring/validator/UserValidatorTest.java
@@ -12,6 +12,8 @@ import junit.framework.TestCase;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.DataBinder;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 
 import test.spring.beans.User;
 
@@ -27,9 +29,6 @@ public class UserValidatorTest extends TestCase {
 		DataBinder binder = new DataBinder(user);
 		binder.setValidator(new UserValidator());
 
-//		// bind to the target object
-//		binder.bind(propertyValues);
-
 		// validate the target object
 		binder.validate();
 
@@ -38,24 +37,53 @@ public class UserValidatorTest extends TestCase {
 		assertEquals(results.getAllErrors().size(), 0 );
 	}
 	
-	public void testInvalidUser() {
+	public void testInvalidUserLastName() {
 		User user = new User();
 		user.setFirstName("Justin");
+		user.setPassWord("Abcd");
+		user.setConfirmPassword("Abcd");
 		
 		DataBinder binder = new DataBinder(user);
 		binder.setValidator(new UserValidator());
-
-//		// bind to the target object
-//		binder.bind(propertyValues);
 
 		// validate the target object
 		binder.validate();
 
 		// get BindingResult that includes any validation errors
 		BindingResult results = binder.getBindingResult();
-		assertEquals(results.getAllErrors().size(), 3 );
+		assertEquals(results.getAllErrors().size(), 1 );
+		assertEquals(results.getTarget().getClass(), User.class );
+		ObjectError error = results.getAllErrors().get(0);
+		assertEquals(error.getClass(), FieldError.class );
+		FieldError error2 = (FieldError)error;		
+		assertEquals("lastName should not be empty", error2.getField(), "lastName" );
+		assertEquals("lastName should not be empty", error2.getCode(), "lastName.empty" );
+		
 	}
 	
+	public void testInvalidUserPassword() {
+		User user = new User();
+		user.setFirstName("Justin");
+		user.setLastName("Wu");
+		user.setConfirmPassword("Abcd");
+		
+		DataBinder binder = new DataBinder(user);
+		binder.setValidator(new UserValidator());
+
+		// validate the target object
+		binder.validate();
+
+		// get BindingResult that includes any validation errors
+		BindingResult results = binder.getBindingResult();
+		assertEquals(results.getAllErrors().size(), 1 );
+		assertEquals(results.getTarget().getClass(), User.class );
+		ObjectError error = results.getAllErrors().get(0);
+		assertEquals(error.getClass(), FieldError.class );
+		FieldError error2 = (FieldError)error;		
+		assertEquals("password should not be empty", error2.getField(), "passWord" );
+		assertEquals("password should not be empty", error2.getCode(), "passWord.empty" );
+		
+	}	
 	
 	public void testInvalidUser2() {
 		User user = new User();
@@ -68,6 +96,10 @@ public class UserValidatorTest extends TestCase {
 		validator.validate(user, errors);
 
 		assertEquals(errors.getFieldErrorCount(), 3 );
+		
+		for(ObjectError error: errors.getAllErrors()){
+			System.out.println(error.getObjectName());
+		}
 	}
 	
 	public void testValidUserOnJSR303() {
@@ -83,13 +115,18 @@ public class UserValidatorTest extends TestCase {
 	
 	public void testInvalidUserOnJSR303() {
 		User user = new User();
-		user.setLastName("Justin719");	// can't have number
+		user.setLastName("Justin719");	
 		
 		ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();		
 		Validator validator = validatorFactory.getValidator();		
 		Set<ConstraintViolation<User>> violations = validator.validate(user);
 
-		assertEquals(violations.size(), 1 );
+		assertEquals("lastName can't have number", violations.size(), 1 );
+		ConstraintViolation<User> result = violations.iterator().next();
+		 
+		assertEquals(result.getRootBean().getClass(), User.class );
+		assertEquals(result.getMessage(), "Last name has invalid characters" );
+		
 	}
 
 }
