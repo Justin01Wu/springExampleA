e9913bd28802aa04708cbeab4f2c1854d791a1e0
Justin Wu <justin01.wu@gmail.com>
2015-01-16 17:31:22 -0500

    finish readOnly Property Demo

--- restful/src/main/webapp/resources/readOnly.js
+++ restful/src/main/webapp/resources/readOnly.js
@@ -6,21 +6,36 @@ function submitAjax(){
 	var data = $("textarea#wrongJson")[0].value;
 	console.log(data);
 	
-	//$("textarea#response").val("sss");
 	
-	//var url = "readOnlyProp/99";
-	var url = "http://localhost:8080/restful/readOnlyProp/99";
+	var url = "readOnlyProp/99";
+	
+	//var url = "http://localhost:8080/restful/readOnlyProp/99";
+	// please add --disable-web-security to chrome command for corss domain testing
+	
 	$.ajax({
 	    url: url,
+	    accepts: {json: 'application/json'},
+	    dataType: 'json',  // expect server return type, ie 'Content-Type'n response
 	    type: "PUT",
-	    data: JSON.stringify(data),
+	    data: data,
 	    contentType: "application/json",
-	    complete: _callBack
+	    error: _errorHandle,
+	    success: _successHandle
 	});
 }
 
-function _callBack(x,y,z){
-	$("textarea#response").val("sss");
+function _errorHandle(jqXHR, textStatus, errorThrown ){
+	console.log("_errorHandle...");
+	var errorMsg = JSON.stringify(jqXHR.responseJSON, null, "\t"); // Indented with tab
+	$("textarea#response").val(errorMsg);
 	
 }
+
+function _successHandle(data, textStatus, jqXHR ){
+	console.log("_successHandle...");
+	var returnMsg = JSON.stringify(jqXHR.responseJSON, null, 4); // Indented 4 spaces
+	$("textarea#response").val(returnMsg);
+	
+}
+
 
